215098774bbf12d3f2c672979c2d61e5674ac9c7
bitterlucky <925182053@qq.com>
2024-10-05 10:44:59 +0800

    disc05

--- discussion/disc04.py
+++ discussion/disc04.py
@@ -64,3 +64,6 @@ def sums(n, m):
     for k in range(1, m + 1):
         result = result + [[k] + rest for rest in sums(n - k, m) if rest == [] or rest[0] != k]
     return result
+
+
+
--- discussion/test.py
+++ discussion/test.py
@@ -1,2 +1,4 @@
-from disc04 import *
-print(sums(5, 2))
+from disc05 import *
+t2 = tree(5, [tree(6), tree(7)])
+t1 = tree(3, [tree(4), t2])
+print(has_path(t2, [5, 6]))
