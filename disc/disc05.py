be9be1e1e77db317cd83f4654138d8fafc8bfb77
bitterlucky <925182053@qq.com>
2025-02-08 10:49:52 +0800

    disc05

--- disc/disc05.py
+++ disc/disc05.py
@@ -20,5 +20,54 @@ def is_tree(tree):
             return False
     return True
 
-t2 = tree(5, [tree(6), tree(7)])
-t1 = tree(3, [tree(4), t2])
+
+def has_path(t, p):
+    """Return whether tree t has a path from the root with labels p.
+    >>> t2 = tree(5, [tree(6), tree(7)])
+    >>> t1 = tree(3, [tree(4), t2])
+    >>> has_path(t1, [5, 6]) # This path is not from the root of t1
+    False
+    >>> has_path(t2, [5, 6]) # This path is from the root of t2
+    True
+    >>> has_path(t1, [3, 5]) # This path does not go to a leaf, but that's ok
+    True
+    >>> has_path(t1, [3, 5, 6]) # This path goes to a leaf
+    True
+    >>> has_path(t1, [3, 4, 5, 6]) # There is no path with these labels
+    False
+    """
+    if label(t) == p[0] and len(p) == 1:
+        return True
+    elif label(t) != p[0]:
+        return False
+    else:
+        for b in branches(t):
+            if (has_path(b, p[1:])):
+                return True
+        return False
+
+def find_path(t, x):
+    """
+    >>> t2 = tree(5, [tree(6), tree(7)])
+    >>> t1 = tree(3, [tree(4), t2])
+    >>> find_path(t1, 5)
+    [3, 5]
+    >>> find_path(t1, 4)
+    [3, 4]
+    >>> find_path(t1, 6)
+    [3, 5, 6]
+    >>> find_path(t2, 6)
+    [5, 6]
+    >>> print(find_path(t1, 2))
+    None
+    """
+    if label(t) == x:
+        return [x]
+    elif label(t) != x:
+        if is_leaf(t):
+            return None
+        else:
+            for b in branches(t):
+                val = find_path(b, x)
+                if val:
+                    return [label(t)] + list(val)
