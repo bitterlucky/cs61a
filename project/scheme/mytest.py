dcd85d64e655b57b554713d13a959b4a3a88f26d
bitterlucky <925182053@qq.com>
2025-02-12 14:54:02 +0800

    scheme all

--- project/scheme/mytest.py
+++ project/scheme/mytest.py
@@ -1,4 +1,10 @@
-# from scheme_reader import *
-from scheme import *
-expr = Pair("+", Pair(2, Pair(2, nil)))
-scheme_eval(expr, create_global_frame())
+def enumerate(s):
+    def inner(s, n):
+        if len(s) == 0:
+            return []
+        else:
+            lst = [[n, s[0]]]
+            lst.extend(inner(s[1:], n + 1))
+            return lst
+    return inner(s, 0)
+print(enumerate([3,4,5,6]))
